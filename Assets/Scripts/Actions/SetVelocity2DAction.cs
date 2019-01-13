2ae506644ab27a1c1afa9d3bf548ead29a7d89b5
Sarah Herzog <sarah.ann.herzog@gmail.com>
2019-01-13 20:39:42 +0000

    Added: - SetVelocity2DAction: Sets a Rigidbody2D's velocity when triggered. - InputAxisActivator: Calls an action function based on axis changes and passes along the axis value

---
+++ Assets/Scripts/Actions/SetVelocity2DAction.cs
@@ -0,0 +1,102 @@
+﻿// -----------------------------------------------------------------------------
+#region File Info - SetVelocity2DAction.cs
+// -----------------------------------------------------------------------------
+// Project:     Drag N Drop Toolkit
+// Created:     Sarah Herzog 2019
+// Purpose:     Sets a Rigidbody2D's velocity
+// -----------------------------------------------------------------------------
+#endregion
+// -----------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------
+#region Libraries
+// -----------------------------------------------------------------------------
+using UnityEngine;
+// -----------------------------------------------------------------------------
+#endregion
+// -----------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------
+#region Component: SetVelocity2DAction
+// -----------------------------------------------------------------------------
+[RequireComponent(typeof(Rigidbody2D))]
+[HelpURL("https://github.com/CodingDino/FifeCollege-Unity-DragNDrop/wiki/SetVelocity2DAction")]
+public class SetVelocity2DAction : MonoBehaviour
+{
+
+
+    // -------------------------------------------------------------------------
+    #region Editor Variables
+    // -------------------------------------------------------------------------
+    [Tooltip("Multiplier used to alter velocity sent from Activators. Use 1 to leave velocities unaltered.")]
+    public float speed = 1.0f;
+    // -------------------------------------------------------------------------
+    #endregion
+    // -------------------------------------------------------------------------
+
+
+    // -------------------------------------------------------------------------
+    #region Internal Variables
+    // -------------------------------------------------------------------------
+    private Rigidbody2D rigidBody = null;
+    // -------------------------------------------------------------------------
+    #endregion
+    // -------------------------------------------------------------------------
+
+
+    // -------------------------------------------------------------------------
+    #region Unity Functions
+    // -------------------------------------------------------------------------
+    private void Start()
+    {
+        // Get the Rigidbody2D component from our GameObject 
+        // and store it for later use
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    // -------------------------------------------------------------------------
+    #endregion
+    // -------------------------------------------------------------------------
+
+
+    // -------------------------------------------------------------------------
+    #region Public Functions
+    // -------------------------------------------------------------------------
+    public void ActionSetVelocity(Vector2 newVelocity)
+    {
+        // Apply the new velocity to our rigidbody
+        // Scale by speed value
+        rigidBody.velocity = newVelocity * speed;
+    }
+    // -------------------------------------------------------------------------
+    public void ActionSetXVelocity(float newXVelocity)
+    {
+        // Get the current velocity from our rigidbody, and store it in a vector
+        Vector2 newVelocity = rigidBody.velocity;
+        // Update the new velocity with the x portion that was passed in
+        // Scale by speed value
+        newVelocity.x = newXVelocity * speed;
+        // Apply the new velocity to our rigidbody
+        rigidBody.velocity = newVelocity;
+    }
+    // -------------------------------------------------------------------------
+    public void ActionSetYVelocity(float newYVelocity)
+    {
+        // Get the current velocity from our rigidbody, and store it in a vector
+        Vector2 newVelocity = rigidBody.velocity;
+        // Update the new velocity with the y portion that was passed in
+        // Scale by speed value
+        newVelocity.y = newYVelocity * speed;
+        // Apply the new velocity to our rigidbody
+        rigidBody.velocity = newVelocity;
+    }
+    // -------------------------------------------------------------------------
+    #endregion
+    // -------------------------------------------------------------------------
+
+
+}
+// -----------------------------------------------------------------------------
+#endregion
+// -----------------------------------------------------------------------------
---
+++ Assets/Scripts/Activators/InputAxisActivator.cs
@@ -0,0 +1,112 @@
+﻿// -----------------------------------------------------------------------------
+#region File Info - InputAxisActivator.cs
+// -----------------------------------------------------------------------------
+// Project:     Drag N Drop Toolkit
+// Created:     Sarah Herzog 2019
+// Purpose:     Perform actions based on axis input
+// -----------------------------------------------------------------------------
+#endregion
+// -----------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------
+#region Libraries
+// -----------------------------------------------------------------------------
+using UnityEngine;
+using UnityEngine.Events;
+// -----------------------------------------------------------------------------
+#endregion
+// -----------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------
+#region Component: InputAxisActivator
+// -----------------------------------------------------------------------------
+[HelpURL("https://github.com/CodingDino/FifeCollege-Unity-DragNDrop/wiki/InputAxisActivator")]
+public class InputAxisActivator : MonoBehaviour
+{
+    // -------------------------------------------------------------------------
+    #region Custom Unity Event: AxisAxtion
+    // -------------------------------------------------------------------------
+    [System.Serializable]
+    public class AxisAction : UnityEvent<float> { }
+    // -------------------------------------------------------------------------
+    #endregion
+    // -------------------------------------------------------------------------
+
+
+    // -------------------------------------------------------------------------
+    #region Editor Variables
+    // -------------------------------------------------------------------------
+
+    // -------------------------------------------------------------------------
+    [Header("Conditions")]
+    // -------------------------------------------------------------------------
+
+    [Tooltip("The name of the axis the player must use to activate these actions. Axis must be set up in InputManager.")]
+    public string axisName;
+
+    // -------------------------------------------------------------------------
+
+    [Header("Actions")]
+    [Tooltip("Perform these actions when the axis changes from its previous value.")]
+    public AxisAction onAxisChanged;
+    [Tooltip("Perform these actions when the axis becomes non-zero.")]
+    public AxisAction onAxisStart;
+    [Tooltip("Perform these actions when the axis becomes zero.")]
+    public AxisAction onAxisRest;
+
+    // -------------------------------------------------------------------------
+    #endregion
+    // -------------------------------------------------------------------------
+
+
+    // -------------------------------------------------------------------------
+    #region Internal Variables
+    // -------------------------------------------------------------------------
+    private float previousAxisValue = 0;
+    // -------------------------------------------------------------------------
+    #endregion
+    // -------------------------------------------------------------------------
+
+
+    // -------------------------------------------------------------------------
+    #region Unity Functions
+    // -------------------------------------------------------------------------
+    private void Update()
+    {
+        // Get the current value for the axis
+        float axisValue = Input.GetAxis(axisName);
+
+        // Has the axis value changed?
+        if (axisValue != previousAxisValue)
+        {
+            // The axis has changed since last frame, so perform the actions.
+            onAxisChanged.Invoke(axisValue);
+        }
+
+        // Did the axis just become active (non-zero)?
+        if (previousAxisValue == 0 && axisValue != 0)
+        {
+            // The axis just became non-zero, so perform actions.
+            onAxisStart.Invoke(axisValue);
+        }
+
+        // Did the axis just become inactive (zero)?
+        if (previousAxisValue != 0 && axisValue == 0)
+        {
+            // The axis just became zero, so perform actions.
+            onAxisRest.Invoke(axisValue);
+        }
+
+        // The current value for the axis now becomes the previous value 
+        // for the next frame
+        previousAxisValue = axisValue;
+    }
+    // -------------------------------------------------------------------------
+    #endregion
+    // -------------------------------------------------------------------------
+}
+// -----------------------------------------------------------------------------
+#endregion
+// -----------------------------------------------------------------------------
