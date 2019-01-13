77ecb714f110777cded96cd33e3264527d6a7884
Sarah Herzog <sarah.ann.herzog@gmail.com>
2019-01-13 17:39:30 +0000

    Added: - ShowInInspectorIf - Attribute for hiding properties based on second bool property value - ShowOnEnum - Attribute for hiding properties based on second enum property value

---
+++ Assets/Scripts/Inspector/ShowInInspectorIf.cs
@@ -0,0 +1,111 @@
+﻿// -----------------------------------------------------------------------------
+#region File Info - ShowInInspectorIf.cs
+// -----------------------------------------------------------------------------
+// Project:     Drag N Drop Toolkit
+// Created:     Sarah Herzog 2019
+// Purpose:     Property drawer for conditionally hiding in the inspector
+// -----------------------------------------------------------------------------
+#endregion
+// -----------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------
+#region Libraries
+// -----------------------------------------------------------------------------
+using UnityEngine;
+using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+// -----------------------------------------------------------------------------
+#endregion
+// -----------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------
+#region PropertyAttribute: ShowInInspectorIf
+// -----------------------------------------------------------------------------
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Struct, Inherited = true)]
+public class ShowInInspectorIf : PropertyAttribute 
+{
+	public string conditionalSourceField = "";
+	public bool compare = true;
+
+	public ShowInInspectorIf(string _conditionalSourceField)
+	{
+		conditionalSourceField = _conditionalSourceField;
+		compare = true;
+	}
+
+	public ShowInInspectorIf(string _conditionalSourceField, bool _compare)
+	{
+		conditionalSourceField = _conditionalSourceField;
+		compare = _compare;
+	}
+}
+// -----------------------------------------------------------------------------
+#endregion
+// -----------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------
+#region PropertyDrawer: ShowInInspectorIfProperyDrawer
+// -----------------------------------------------------------------------------
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(ShowInInspectorIf))]
+public class ShowInInspectorIfProperyDrawer : PropertyDrawer 
+{
+	public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label)
+	{
+		ShowInInspectorIf showInInspector = (ShowInInspectorIf)attribute;
+		bool enabled = ShouldShow(showInInspector, _property);
+		bool wasEnabled = GUI.enabled;
+		GUI.enabled = enabled;
+		if (enabled)
+		{
+			++EditorGUI.indentLevel;
+			EditorGUI.PropertyField(_position, _property, _label, true);
+			--EditorGUI.indentLevel;
+		}
+
+		GUI.enabled = wasEnabled;
+	}
+
+	private bool ShouldShow(ShowInInspectorIf _attribute, SerializedProperty _property)
+	{
+		bool enabled = true;
+		string propertyPath = _property.propertyPath;
+		string conditionPath = propertyPath.Replace(_property.name, _attribute.conditionalSourceField);
+		SerializedProperty sourcePropertyValue = _property.serializedObject.FindProperty(conditionPath);
+
+		if (sourcePropertyValue != null)
+		{
+			enabled = sourcePropertyValue.boolValue == _attribute.compare;
+		}
+		else
+		{
+			Debug.LogWarning("Attempting to use ShowInInspectorIf but no matching SourcePropertyValue found in object: "+_attribute.conditionalSourceField);
+		}
+
+		return enabled;
+	}
+
+	public override float GetPropertyHeight(SerializedProperty _property, GUIContent _label)
+	{
+		ShowInInspectorIf showInInspector = (ShowInInspectorIf)attribute;
+		bool enabled = ShouldShow(showInInspector, _property);
+
+		if (enabled)
+		{
+			return EditorGUI.GetPropertyHeight(_property, _label);
+		}
+		else
+		{
+			return -EditorGUIUtility.standardVerticalSpacing;
+		}
+	}
+}
+#endif
+// -----------------------------------------------------------------------------
+#endregion
+// -----------------------------------------------------------------------------
