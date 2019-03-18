91bb1c5f4e460ece306d5e03dc1c16de665aef89
Sarah Herzog <sarah.ann.herzog@gmail.com>
2019-03-18 15:39:15 +0000

    - Added ScaleAction

---
+++ Assets/DinoToolkit/Scripts/Actions/ScaleAction.cs
@@ -0,0 +1,63 @@
+﻿// -----------------------------------------------------------------------------
+#region File Info - ScaleAction.cs
+// -----------------------------------------------------------------------------
+// Project:     Dino Unity Toolkit
+// Created:     Sarah Herzog 2019
+// Purpose:     Scales an object
+// -----------------------------------------------------------------------------
+#endregion
+// -----------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------
+#region Libraries
+// -----------------------------------------------------------------------------
+using UnityEngine;
+// -----------------------------------------------------------------------------
+#endregion
+// -----------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------
+#region Component: ScaleAction
+// -----------------------------------------------------------------------------
+[HelpURL("https://github.com/CodingDino/FifeCollege-Unity-DragNDrop/wiki/ScaleAction")]
+public class ScaleAction : MonoBehaviour
+{
+    // -------------------------------------------------------------------------
+    #region Action Functions
+    // -------------------------------------------------------------------------
+    public void ActionSetScaleX(float scaleX)
+    {
+        transform.localScale = new Vector3( scaleX,
+                                            transform.localScale.y,
+                                            transform.localScale.z);
+    }
+    // -------------------------------------------------------------------------
+    public void ActionSetScaleY(float scaleY)
+    {
+        transform.localScale = new Vector3( transform.localScale.x,
+                                            scaleY,
+                                            transform.localScale.z);
+    }
+    // -------------------------------------------------------------------------
+    public void ActionScaleXBy(float scaleX)
+    {
+        transform.localScale = new Vector3( transform.localScale.z * scaleX,
+                                            transform.localScale.y,
+                                            transform.localScale.z);
+    }
+    // -------------------------------------------------------------------------
+    public void ActionScaleYBy(float scaleY)
+    {
+        transform.localScale = new Vector3( transform.localScale.x,
+                                            transform.localScale.y * scaleY,
+                                            transform.localScale.z);
+    }
+    // -------------------------------------------------------------------------
+    #endregion
+    // -------------------------------------------------------------------------
+}
+// -----------------------------------------------------------------------------
+#endregion
+// -----------------------------------------------------------------------------
